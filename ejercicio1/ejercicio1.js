896c508e42e936dfa80b0446f38b0e8413309093
Silvia <65729100+SilSuvi@users.noreply.github.com>
2022-01-25 12:40:30 +0100

    Update ejercicio1.js

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,19 +1,13 @@
-let personajes = "";
-
-fetch("https://rickandmortyapi.com/api/character")
-  .then(function recibirRespuesta(respuesta) {
+fetch ("https://rickandmortyapi.com/api/character")
+    .then(function cogerRespuesta(respuesta){
     return respuesta.json();
-  })
-  .then(function cogerDatos(datos) {
-    for (let i = 0; i < datos.results.length; i++) {
-      personajes =
-        personajes +
-        `
-            <div>
-                <img src="${datos.results[i].image}" alt""/>
-                <h1>${datos.results[i].name}</h1>
-            </div>
-        `;
+    }).then(function cogerDatos(datos){
+    for(let i=0;i<datos.results.length; i++){
+    document.getElementById("practica").innerHTML+=`
+    <div>
+    <p>${datos.results[i].name}</p>
+    <p><img src="${datos.results[i].image}"></p>
+    </div>
+    `
     }
-    document.getElementById("div1").innerHTML = personajes;
-  });
+    })
